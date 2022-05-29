f8e24d7ad05001801c20b84f252a0e16a12a8716
hazelnutcloud <ha5anmukhli50@gmail.com>
2022-05-29 12:59:04 +0800

    added prelude

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
 #![feature(div_duration)]
 pub mod snapshot_interpolation;
 pub mod vault;
+
+pub mod prelude {
+    use super::*;
+    pub use snapshot_interpolation::SnapshotInterpolation;
+    pub use vault::Vault;
+}
